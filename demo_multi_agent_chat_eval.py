d3f8f10e293286055bd7055f94e1a3ab8a7223b5
axfbh <501404164@qq.com>
2026-03-12 23:02:07 +0800

    upload

--- demo_multi_agent_chat_eval.py
+++ demo_multi_agent_chat_eval.py
@@ -3,3 +3,46 @@ import torch
 import safetensors.torch as safetorch
 from tqdm import tqdm
 from datasets import Dataset
+from transformers import TrainerArguments, Trainer, DataCollatorForSeq2Seq
+from peft import LoraConfig, get_peft_model, PeftModel
+from modelscope import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Qwen/Qwen3-1.7B"
+output_dir = "./qwen_lora_finetuned"
+max_length = 384
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+    use_cache=True,
+)
+
+print("base model load successful!")
+
+model = PeftModel.from_pretrained(model, output_dir)
+
+messages = [
+    {"role": "user", "content": "我没收到货呢所以就想知道是哪里的问题？"}
+]
+
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+)
+
+model_inputs = tokenizer(text, return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=max_length,
+)
+
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+responses = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(responses)
